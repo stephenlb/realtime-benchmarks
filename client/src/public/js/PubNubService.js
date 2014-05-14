a51e5075e9ab954ef6b96ad084e807fb4f543159
Stephen Blum <blum.stephen@gmail.com>
2014-05-14 10:27:15 -0700

    Updated PubNubService.js code a bit for neatness purposes.

--- client/src/public/js/PubNubService.js
+++ client/src/public/js/PubNubService.js
@@ -1,55 +1,49 @@
-/**
- * 
- */
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// PubNub Global RTN (Real Time Network) - Realtime IoT/Mobile/Web
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// 
+// Phil Leggetter Super Benchmark Test of Excellence and Awesome
+// 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 function PubNubService() {
-  BenchmarkService.apply( this, arguments );
-  
-  this.name = 'PubNub';
+    BenchmarkService.apply( this, arguments );
 
-  this.pubnub = PUBNUB.init( {
-    publish_key: 'pub-a1c89fc1-c4a0-4ac9-a784-d42824e1c0eb',
-    subscribe_key: 'sub-be77f868-1ee3-11e2-b53c-85075d7b5343'
-  } );
-  
-  var self = this;
-  
-  self.pubnub.subscribe({
-    channel    : self._channelName,
+    // This is PubNub so Let's get Started with Initialization
+    var self    = this;
+    self.name   = 'PubNub';
+    self.pubnub = PUBNUB({
+        publish_key   : 'pub-a1c89fc1-c4a0-4ac9-a784-d42824e1c0eb',
+        subscribe_key : 'sub-be77f868-1ee3-11e2-b53c-85075d7b5343'
+    });
 
-    restore    : false,              // STAY CONNECTED, EVEN WHEN BROWSER IS CLOSED
-                                     // OR WHEN PAGE CHANGES.
-
-    callback   : function(message) {
-      self._onMessage( message );
-    },
-
-    disconnect : function() {        // LOST CONNECTION.
-      self._log(
-        "Connection Lost." +
-        "Will auto-reconnect when Online."
-      );
-    },
-
-    reconnect  : function() {        // CONNECTION RESTORED.
-      self._log("And we're Back!")
-    },
-
-    connect    : function() {
-    	self._onReady();
-    }
-  });
+    // Start Connection
+    self.pubnub.subscribe({
+        channel    : self._channelName,
+        message    : function(msg) { self._onMessage(msg) },
+        connect    : function()    { self._onReady() },
+        disconnect : function()    { self._log("Connection Done.") },
+        reconnect  : function()    { self._log("And we're Back!") }
+    });
 }
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Inherit BenchmarkService's Methods
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 PubNubService.prototype = new BenchmarkService;
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Send Message
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 PubNubService.prototype.send = function( data ) {
-	this.pubnub.publish({
-	  channel : this._channelName,
-	  message : data
-	});
+    this.pubnub.publish({
+        channel : this._channelName,
+        message : data
+    });
 };
 
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Disconnect
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 PubNubService.prototype.disconnect = function( data ) {
-  this.pubnub.unsubscribe({
-		channel : this._channelName
-  });
+    this.pubnub.unsubscribe({ channel : this._channelName });
 };
